05adefc6b680365d55363cf814428d1008bce3ff
Chandradeep Chowdhury <chandradeep.choudhury@gmail.com>
2023-04-08 18:30:03 -0700

    skeleton completed

--- src/main.py
+++ src/main.py
@@ -1,42 +1,47 @@
 
-class Recommender:
-    scale_encoder = {
+class MonitorRecommender:
+    _scale_encoder = {
         'no idea': 0,
         'never': 1,
         'sometimes': 2,
         'frequently': 3,
         'mostly': 4,
-        'only': 5
+        'only': 5,
+        'yes': True,
+        'no': False
+
     }
 
     '''Input format:
     {
-    'budget' = 
-    'comp' =
-    'sdrcas' = 
-    'hdrcas' =
-    'text' =
-    'sdrmov' =
-    'hdrmov' =
-    'digpic' = 
-    'printpic' =
-    'sdrvid' =
-    'hdrvid' = 
+    'budget':
+    'comp':
+    'sdrcas': 
+    'hdrcas':
+    'text':
+    'sdrmov':
+    'hdrmov':
+    'digpic': 
+    'printpic':
+    'sdrvid':
+    'hdrvid':
+    'secondary':
     }
     '''
 
     def __init__(self, input):
         self._budget = input['budget']
-        self._comp = Recommender.scale_encoder[input['comp']]
-        self._sdrcas = Recommender.scale_encoder[input['sdrcas']]
-        self._hdrcas = Recommender.scale_encoder[input['hdrcas']]
-        self._text = Recommender.scale_encoder[input['text']]
-        self._sdrmov = Recommender.scale_encoder[input['sdrmov']]
-        self._hdrmov = Recommender.scale_encoder[input['hdrmov']]
-        self._digpic = Recommender.scale_encoder[input['digpic']]
-        self._printpic = Recommender.scale_encoder[input['printpic']]
-        self._sdrvid = Recommender.scale_encoder[input['sdrvid']]
-        self._hdrvid = Recommender.scale_encoder[input['hdrvid']]
+        self._comp = MonitorRecommender._scale_encoder[input['comp']]
+        self._sdrcas = MonitorRecommender._scale_encoder[input['sdrcas']]
+        self._hdrcas = MonitorRecommender._scale_encoder[input['hdrcas']]
+        self._text = MonitorRecommender._scale_encoder[input['text']]
+        self._sdrmov = MonitorRecommender._scale_encoder[input['sdrmov']]
+        self._hdrmov = MonitorRecommender._scale_encoder[input['hdrmov']]
+        self._digpic = MonitorRecommender._scale_encoder[input['digpic']]
+        self._printpic = MonitorRecommender._scale_encoder[input['printpic']]
+        self._sdrvid = MonitorRecommender._scale_encoder[input['sdrvid']]
+        self._hdrvid = MonitorRecommender._scale_encoder[input['hdrvid']]
+        self._secondary = MonitorRecommender._scale_encoder[input['secondary']]
         
     def recommend(self):
         pass
