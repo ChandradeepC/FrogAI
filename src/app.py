411def75c74263037414b5f3ecd630b80464b586
Chandradeep Chowdhury <chandradeep.choudhury@gmail.com>
2023-04-22 17:41:07 -0700

    added flask server

---
+++ src/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+from main import MonitorRecommender, Monitor
+
+app = Flask(__name__)
+
+
+@app.route("/api/monitor-recommendations", methods=["POST"])
+def monitor_recommendations():
+    input_data = request.json
+    recommender = MonitorRecommender(input_data)
+
+    return jsonify([m.__dict__ for m in recommender.recommend()])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
