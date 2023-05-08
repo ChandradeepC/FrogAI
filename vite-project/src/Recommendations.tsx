8844225cc9a5324b66b83b16f87cc6abbf89f38d
Chandradeep Chowdhury <chandradeep.choudhury@gmail.com>
2023-05-08 10:29:51 -0700

    relative api calls

--- vite-project/src/Recommendations.tsx
+++ vite-project/src/Recommendations.tsx
@@ -133,7 +133,7 @@ const RecommendationForm: React.FC<Props> = ({
             hub
         };
 
-        fetch('http://127.0.0.1:8000/api/monitor-recommendations', {
+        fetch('/api/monitor-recommendations', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
